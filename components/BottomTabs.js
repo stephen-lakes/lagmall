f47a1b216e1dabf4dcf5d3d4ec8f4ee7b0344d97
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-10-03 14:12:11 +0100

    added icons

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather, MaterialIcons, SimpleLineIcons } from '@expo/vector-icons';
+import React from 'react';
 
 const BottomTabs = () => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>home</Text>
-        <Text>cart</Text>
-        <Text>categories</Text>
-        <Text>account</Text>
+        <TouchableOpacity style={styles.navLink}>
+          <MaterialIcons name='storefront' size={24} color='black' />
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navLink}>
+        <View style={styles.badge}>
+          <Text style={styles.badgeText}>0</Text>
+        </View>
+        <SimpleLineIcons name='bag' size={24} color='#333333' />
+        <Text>Cart</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navLink}>
+        <Feather name='heart' size={24} color='black' />
+        <Text>WishList</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navLink}>
+        <Feather name='user' size={24} color='black' />
+        <Text>My account</Text>
+      </TouchableOpacity>
       </View>
     </View>
     
@@ -18,6 +34,22 @@ const BottomTabs = () => {
 export default BottomTabs;
 
 const styles = StyleSheet.create({
+  badge: {
+    backgroundColor: '#ff3250',
+    position: 'absolute',
+    left: 13,
+    bottom: 10,
+    width: 17,
+    height: 17,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 100,
+  },
+  badgeText: {
+    color: 'white',
+    fontWeight: '600'
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
@@ -26,6 +58,9 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     borderTopWidth: 1,
     borderTopColor: 'gray',
+  },
+  navLink: {
+    alignItems: 'center',
   },
   wrapper: {
     position: 'fixed',
