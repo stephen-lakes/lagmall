80f32e5e7a78631522aa05a96295f69af2d83231
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-10-16 20:17:03 +0100

    added a file to load in the banner images

---
+++ data/Banner.js
@@ -0,0 +1,12 @@
+export const BANNER = [
+    {
+        imageUrl: require('../assets/NikeBanner.jpg'),
+    },
+    {
+        imageUrl: require('../assets/MenBanner.jpg'),
+    },
+    {
+        imageUrl: require('../assets/FitnessBanner.jpg'),
+    },
+    
+]
