d721fde1e1cb9e2a6c163ef246fb9a0015bf2e3c
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-10-16 20:13:38 +0100

    imported components and data needed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,25 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 import BottomTabs from '../components/BottomTabs';
 import Header from '../components/home/Header';
+import SlideShow from '../components/home/SlideShow';
+import PlainTees from '../components/home/PlainTees';
+import CustomRow from '../components/home/CustomRow';
+import Banner from '../components/Banner';
+
+import { BANNER } from '../data/Banner';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
         <View>
             <Header />
-            {/* body */}
+            <SlideShow />
+            <CustomRow />
+            <PlainTees />
+            <Banner path={BANNER[0]} />
+            <Banner path={BANNER[1]} />
             <BottomTabs />
         </View>
     </SafeAreaView>
@@ -19,4 +29,6 @@ const HomeScreen = () => {
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
