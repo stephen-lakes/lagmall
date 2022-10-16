3675761eaa89cc99413d6b0c2df3593880bded5f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-10-16 19:47:30 +0100

    added data for the t shirts

--- data/Tees.js
+++ data/Tees.js
@@ -3,21 +3,25 @@ export const PLAINTEES = [
         name: 'Libra T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/yelloTees300x300.jpg'),
+        id: 1,
     },
     {
         name: 'A pack of 3 plain T-Shirt',
         price: '6500.00',
         imageUrl: require('../assets/girl-boss-2500-300x300.jpg'),
+        id: 2,
     },
     {
         name: 'Libra T-Shirt',
         price: '1550.00',
         imageUrl: require('../assets/yelloTees300x300.jpg'),
+        id: 3,
     },
     {
         name: 'A pack of 5 plain T-Shirt',
         price: '8800.00',
         imageUrl: require('../assets/girl-boss-2500-300x300.jpg'),
+        id: 4,
     },
     {
         name: 'Gemini T-Shirt',
@@ -28,25 +32,30 @@ export const PLAINTEES = [
         name: 'Saggitarius T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/girl-boss-2500-300x300.jpg'),
+        id: 5,
     },
     {
         name: 'Girl Boss T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/yelloTees300x300.jpg'),
+        id: 6,
     },
     {
         name: 'Capricorn T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/girl-boss-2500-300x300.jpg'),
+        id: 7,
     },
     {
         name: 'Cancer T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/yelloTees300x300.jpg'),
+        id: 8,
     },
     {
         name: 'Aquarius T-Shirt',
         price: '3100.00',
         imageUrl: require('../assets/girl-boss-2500-300x300.jpg'),
+        id: 9,
     },
 ]
