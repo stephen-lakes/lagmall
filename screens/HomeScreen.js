3a9dd2812c11a78e70b6b0275257872b6be6897a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-10-17 08:30:42 +0100

    git modified the following screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,7 +23,6 @@ const HomeScreen = () => {
       </View>
       <BottomTabs />
     </SafeAreaView>
-    
   )
 }
 
---
+++ screens/Whislist.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Whislist = () => {
+  return (
+    <SafeAreaView>
+      <Header />
+      <View style={{ marginTop: 50 }}>
+
+      </View>
+      <BottomTabs />
+    </SafeAreaView>
+  )
+}
+
+export default Whislist
+
+const styles = StyleSheet.create({})
